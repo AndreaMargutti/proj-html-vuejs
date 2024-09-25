0fc665a10b24a8a53692868f772f03a8ab1afe6c
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-25 17:27:08 +0200

    first commit / Add button: read more

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,20 @@
 <script>
+import ReadMoreBtn from "./ReadMoreBtn.vue";
 export default {
   data() {
-    return {
-      
-    }
-  }
-}
+    return {};
+  },
+  components: {
+    ReadMoreBtn,
+  },
+};
 </script>
 
 <template>
-    <h1>AppHeader</h1>
+  <h1>AppHeader</h1>
+  <ReadMoreBtn />
 </template>
 
 <style scoped>
+
 </style>
---
+++ src/components/AppMainSenseTheJazz.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="main-jazz">
+    <div></div>
+  </div>
+</template>
+
+<style scoped></style>
