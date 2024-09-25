56b0c6e21a3b1e2a59adbc3bd06a9782dee9c6f8
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-25 17:24:46 +0200

    added two sub components

--- src/components/AppMainBottom.vue
+++ src/components/AppMainBottom.vue
@@ -1,17 +1,33 @@
 <script>
+import AppMainBottomBlogPost from './AppMainBottomBlogPost.vue';
+import AppMainBottomCarousel from './AppMainBottomCarousel.vue';
 export default {
   data() {
     return {
       
     }
+  },
+  components:{
+    AppMainBottomBlogPost,
+    AppMainBottomCarousel
   }
 }
 </script>
 
 <template>
-    <h2>AppMainBottom</h2>
+    <section id="blog">
+        <h3>MUSIC BLOG</h3>
+        <h2>BEST MUSIC BLOG</h2>
+        <AppMainBottomBlogPost/>
+        <AppMainBottomCarousel/>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+#blog{
+    background-color: black;
+    color: #f2870c;
+    height: 200px;
+}
 
 </style>
