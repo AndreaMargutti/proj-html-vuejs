390e7e85dedec72fabd2c327ead3befe08f8d868
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-25 21:15:57 +0200

    added 2 components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,20 @@
 <script>
+import AppMainSenseTheJazz from "./AppMainSenseTheJazz.vue";
+import ReadMoreBtn from "./ReadMoreBtn.vue";
+import AppMainInstrumentalRock from "./AppMainInstrumentalRock.vue";
 export default {
   data() {
-    return {
-      
-    }
-  }
-}
+    return {};
+  },
+  components: { AppMainSenseTheJazz, ReadMoreBtn, AppMainInstrumentalRock },
+};
 </script>
 
 <template>
   <h1>AppMain</h1>
+
+  <AppMainSenseTheJazz />
+  <AppMainInstrumentalRock />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style/general.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).mount("#app");
+// Crea l'app e registra il componente font-awesome-icon globalmente
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
