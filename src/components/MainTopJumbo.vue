33aa153a8de01a1f2f0cbcfb405ce55034334798
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-25 17:19:36 +0200

    added font

--- src/components/MainTopJumbo.vue
+++ src/components/MainTopJumbo.vue
@@ -9,8 +9,30 @@ export default {
 </script>
 
 <template>
-    <h1>MainTopJumbo</h1>
+    <section id="jumbo">
+        <h3>Instrumental Rock</h3>
+        <h1>Music in this video</h1>
+        <h1>Music of the spirit</h1>
+    </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    #jumbo {
+        // Da togliere in seguito
+        height: 30rem;
+        background-color: #000000;
+        
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        //Inizio stile h3
+        h3 {
+            color: #F2870C;
+        }
+        //Inizio stile h1
+        h1 {
+            color: #FFFFFF;
+        }
+    }
 </style>
