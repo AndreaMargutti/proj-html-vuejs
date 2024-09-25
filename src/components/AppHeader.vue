e03762602da5c6e251ee578a0d64723855bc06be
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-25 17:39:51 +0200

    Added 2 components: btn/jazz

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,13 @@
 <script>
-import ReadMoreBtn from "./ReadMoreBtn.vue";
 export default {
   data() {
     return {};
-  },
-  components: {
-    ReadMoreBtn,
   },
 };
 </script>
 
 <template>
   <h1>AppHeader</h1>
-  <ReadMoreBtn />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
