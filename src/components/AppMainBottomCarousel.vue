686adb9d4fb47a399ba326af7fcf0e12ceef4599
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-09-25 22:17:46 +0200

    added a way to display img in the carousel. Now I need to implement the scroll left and right and the effects on time

--- src/components/AppMainBottomCarousel.vue
+++ src/components/AppMainBottomCarousel.vue
@@ -1,4 +1,11 @@
 <script>
+// stabilisco come inizio il primo indice della seconda copia dell'array
+const startIndex = 10;
+// stabilisco come minimo,l'indice iniziale della prima copia dell'array
+const minIndex = 0;
+//stabilisco come max, l'indice iniziale della terza copia dell'array 
+const maxIndex = 20;
+
 export default {
   data() {
     return {
@@ -14,7 +21,7 @@ export default {
         {path:'../assets/img/instagram_img9.jpg', label:'instagram-img9'},
         {path:'../assets/img/instagram_img10.jpg', label:'instagram-img10'},
       ],
-      
+      currentIndex: startIndex,
     }
   },
   methods:{
@@ -22,16 +29,60 @@ export default {
     getImgCarousel(rawPath){
       const imgPath = new URL( rawPath, import.meta.url).href;
       return imgPath;
+    },
+    // vado a gestire lo slide che scorre a destra
+    onArrowRight() {
+      this.currentIndex++;
+      // vado a mettere una condizione di controllo fino a quando
+      // deve andare avanti
+      if (this.currentIndex > maxIndex) {
+        this.currentIndex = startIndex + 1;
+      }
+      console.log("arrowRight", this.currentIndex);
+    },
+    // vado a gestire lo slide che scorre a sinistra
+    onArrowLeft() {
+      this.currentIndex--;
+      if (this.currentIndex < minIndex) {
+        this.currentIndex = startIndex - 1;
+      }
+      console.log("arrowLeft", this.currentIndex);
+    },
+    // vado a clonare il mio array di img in modo da poter
+    // scorrere diverse immagini continuamente nel carousel
+    getImageCarousel() {
+      return [
+        ...this.imgCarousel,
+        ...this.imgCarousel,
+        ...this.imgCarousel
+      ]
+    },
+    getPositionLeft() {
+      // vado a calcolare le misure dello schermo
+      const windowSize = document.querySelector("body").offsetWidth;
+      // creo la variabile del numero di box img visibili in pagina
+      const boxesPerPage = 8;
+      // mi calcolo la width delle box  in base alla width 
+      // dello schermo e arrotodono per eccesso
+      const boxWidth = Math.ceil(windowSize / boxesPerPage);
+      return `-${boxWidth * this.currentIndex}px`; 
     }
   }
 }
 </script>
 
 <template>
-  <div class="main-carousel">
-    <div class="box" v-for="(itemCarousel, index) in imgCarousel" :key="index" >
+  <!-- provo inizialmente con gli eventi al click per scorrere a destra e a sinistra -->
+  <div class="main-carousel" @click.exact="onArrowRight" @click.ctrl="onArrowLeft">
+
+    <div class="box" 
+       v-for="(itemCarousel, index) in getImageCarousel()" 
+       :key="index" 
+       :style="{ left: getPositionLeft()}" 
+       >
       <img :src="getImgCarousel(itemCarousel.path)" :alt="itemCarousel.label">
     </div>
+
   </div>
 </template>
 
@@ -42,7 +93,8 @@ export default {
 // l'altezza dei box sarà la stessa della larghezza ovvero 100vw/numero dei box
 // delle immagini in questo caso 9
 
-$height: calc(100vw / 9);
+$boxes_per_page: 8;
+$height: calc(100vw / $boxes_per_page);
 
 .main-carousel{
   height: $height;
@@ -50,7 +102,8 @@ $height: calc(100vw / 9);
   white-space: nowrap;
   .box{
   height:$height;
-  width: calc(100% / 9);
+  width: calc(100% / $boxes_per_page);
+  position: relative;
   display: inline-block;
     img{
       width: 100%;
@@ -58,5 +111,4 @@ $height: calc(100vw / 9);
     }
   }
 }
-
 </style>
