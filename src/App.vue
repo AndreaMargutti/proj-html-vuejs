75a032b49ce604eb1d622f97356bb96548e7da18
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-25 15:26:02 +0200

    created components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+export default {
+  data() {
+    return {
 
+    }
+  },
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <h1>AppHeader</h1>
+</template>
+
+<style scoped>
+</style>
