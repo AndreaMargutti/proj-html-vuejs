3534416a6b1b8b55cf4820c0f3dab7fe441cf145
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-26 13:17:23 +0200

    fixed header drop-list

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,97 +2,97 @@
 import Navbar from "./Navbar.vue";
 
 export default {
-	name: "AppHeader",
-	components: {
-		Navbar
-	},
-	data: () => ({
-		// Struttura dati
-		pagesList: [
-			{
-				id: 1,
-				name: "Home",
-				url: "#",
-				dropList: []
-			},
-			{
-				id: 2,
-				name: "Blog",
-				url: "#",
-				dropList: []
-			},
-			{
-				id: 3,
-				name: "Events",
-				url: "#",
-				dropList: [
-					{
-						name: "Choral Music",
-						url: "#"
-					},
-					{
-						name: "Concert Band",
-						url: "#"
-					},
-					{
-						name: "Opera Concerts",
-						url: "#"
-					},
-					{
-						name: "Symphony Orchestra",
-						url: "#"
-					},
-					{
-						name: "Family Concerts",
-						url: "#"
-					}
-				]
-			},
-			{
-				id: 4,
-				name: "Gallery",
-				url: "#",
-				dropList: []
-			},
-			{
-				id: 5,
-				name: "About Us",
-				url: "#",
-				dropList: []
-			},
-			{
-				id: 6,
-				name: "Contact Us",
-				url: "#",
-				dropList: []
-			},
-			{
-				id: 7,
-				name: "Shop",
-				url: "#",
-				dropList: [
-					{
-						name: "Product Type",
-						url: "#"
-					},
-					{
-						name: "Shop Page",
-						url: "#"
-					}
-				]
-			}
-		]
-	})
-}
+  name: "AppHeader",
+  components: {
+    Navbar,
+  },
+  data: () => ({
+    // Struttura dati
+    pagesList: [
+      {
+        id: 1,
+        name: "Home",
+        url: "#",
+        dropList: [],
+      },
+      {
+        id: 2,
+        name: "Blog",
+        url: "#",
+        dropList: [],
+      },
+      {
+        id: 3,
+        name: "Events",
+        url: "#",
+        dropList: [
+          {
+            name: "Choral Music",
+            url: "#",
+          },
+          {
+            name: "Concert Band",
+            url: "#",
+          },
+          {
+            name: "Opera Concerts",
+            url: "#",
+          },
+          {
+            name: "Symphony Orchestra",
+            url: "#",
+          },
+          {
+            name: "Family Concerts",
+            url: "#",
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Gallery",
+        url: "#",
+        dropList: [],
+      },
+      {
+        id: 5,
+        name: "About Us",
+        url: "#",
+        dropList: [],
+      },
+      {
+        id: 6,
+        name: "Contact Us",
+        url: "#",
+        dropList: [],
+      },
+      {
+        id: 7,
+        name: "Shop",
+        url: "#",
+        dropList: [
+          {
+            name: "Product Type",
+            url: "#",
+          },
+          {
+            name: "Shop Page",
+            url: "#",
+          },
+        ],
+      },
+    ],
+  }),
+};
 </script>
 
 <template>
-	<!-- Header -->
-	<header>
-		<a href="#"><img src="../assets/img/Logo.png" alt="Lyricsmus"></a>
-		<!-- Navbar -->
-		<Navbar :list="pagesList" />
-	</header>
+  <!-- Header -->
+  <header>
+    <a href="#"><img src="../assets/img/Logo.png" alt="Lyricsmus" /></a>
+    <!-- Navbar -->
+    <Navbar :list="pagesList" />
+  </header>
 </template>
 
 <style lang="scss" scoped>
@@ -100,22 +100,22 @@ export default {
 @use "../style/partials/mixins" as *;
 
 header {
-	height: 100px;
-	background-color: #000;
-	@include flex(row, nowrap);
-	justify-content: space-between;
-	align-items: center;
-	padding: 0 2rem;
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	z-index: 1;
+  height: 100px;
+  background-color: #000;
+  @include flex(row, nowrap);
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
 
-	img {
-		display: block;
-		max-width: 100%;
-		height: 35px;
-	}
+  img {
+    display: block;
+    max-width: 100%;
+    height: 35px;
+  }
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,55 @@
 <script>
 export default {
-    name: "Navbar",
-    props: {
-        list: {
-            type: Object,
-            required: true
-        }
+  name: "Navbar",
+  props: {
+    list: {
+      type: Object,
+      required: true,
     },
-    data: () => ({
-        currentPage: 0
-    })
-}
+  },
+  data: () => ({
+    currentPage: 0,
+  }),
+};
 </script>
 
 <template>
-    <!-- Navbar -->
-    <nav class="navbar p-0">
-        <ul class="navbar-list m-0 h-100">
-            <li class="navbar-list-item h-100" v-for="(item, i) in list" :key="item.id">
-                <a v-if="!item.dropList[0]" :href="item.url" :class="{ active: i === currentPage }"
-                    v-text="item.name"></a>
-                <div v-else class="drop-menu">
-                    <a :href="item.url" :class="{ active: i === currentPage }">{{ item.name }} <i
-                            class="fa-solid fa-angle-down"></i></a>
-                    <ul class="drop-list m-0 p-0">
-                        <li class="drop-list-item" v-for="(item, i) in list[i].dropList" :key="i"><a :href="item.url"
-                                v-text="item.name"></a></li>
-                    </ul>
-                </div>
+  <!-- Navbar -->
+  <nav class="navbar p-0">
+    <ul class="navbar-list m-0 h-100">
+      <li
+        class="navbar-list-item h-100"
+        v-for="(item, i) in list"
+        :key="item.id"
+        :class="{ 'shop-item': item.name === 'Shop' }"
+      >
+        <!-- Aggiungi classe "shop-item" -->
+
+        <a
+          v-if="!item.dropList[0]"
+          :href="item.url"
+          :class="{ active: i === currentPage }"
+          v-text="item.name"
+        ></a>
+
+        <div v-else class="drop-menu">
+          <a :href="item.url" :class="{ active: i === currentPage }"
+            >{{ item.name }} <i class="fa-solid fa-angle-down"></i
+          ></a>
+
+          <ul class="drop-list m-0 p-0">
+            <li
+              class="drop-list-item"
+              v-for="(dropItem, j) in item.dropList"
+              :key="j"
+            >
+              <a :href="dropItem.url" v-text="dropItem.name"></a>
             </li>
-        </ul>
-    </nav>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
@@ -38,60 +57,62 @@ export default {
 @use "../style/partials/mixins.scss" as *;
 
 .navbar {
-    height: 100%;
+  height: 100%;
 
-    .navbar-list {
-        @include flex(row, nowrap);
-        align-items: center;
-        gap: 2rem;
+  .navbar-list {
+    @include flex(row, nowrap);
+    align-items: center;
+    gap: 2rem;
 
-        .drop-list {
-            display: none;
-            width: 250px;
-            background-color: #1F1F1F;
-            position: absolute;
-            top: 100px;
+    .drop-list {
+      display: none;
+      width: 250px;
+      background-color: #1f1f1f;
+      position: absolute;
+      top: 100px;
+      list-style: none;
 
-            li {
-                padding: 1rem;
-            }
+      li {
+        padding: 1rem;
+      }
 
-            li:not(:last-child) {
-                border-bottom: 1px solid #333;
-            }
-        }
-
-        a {
-            color: #FFF;
-            font-weight: bold;
-            text-decoration: none;
-            text-transform: uppercase;
-
-            .active {
-                color: $primary-color;
-            }
-
-            &:hover {
-                color: $primary-color;
-            }
-        }
+      li:not(:last-child) {
+        border-bottom: 1px solid #333;
+      }
     }
-}
 
-.navbar-list-item:hover .drop-list {
+    a {
+      color: #fff;
+      font-weight: bold;
+      text-decoration: none;
+      text-transform: uppercase;
+
+      .active {
+        color: $primary-color;
+      }
+
+      &:hover {
+        color: $primary-color;
+      }
+    }
+  }
+
+  .navbar-list-item:hover .drop-list {
     display: block;
-}
+  }
 
-.navbar-list-item:hover .drop-menu>a {
+  .navbar-list-item:hover .drop-menu > a {
     color: orange;
-}
+  }
 
-.navbar-list-item {
+  .navbar-list-item {
     display: flex;
     align-items: center;
-}
+  }
 
-ul>li:first-child {
-    color: $primary-color;
+  .shop-item .drop-list {
+    right: 0;
+    left: auto;
+  }
 }
 </style>
