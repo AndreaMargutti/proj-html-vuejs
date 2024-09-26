d2b2748417cc83a9b1406f96f96ac52ae9c51acf
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-26 18:08:20 +0200

    fixed size component

--- src/components/MainTopVideo.vue
+++ src/components/MainTopVideo.vue
@@ -1,45 +1,43 @@
 <script>
 export default {
   data() {
-    return {
-      
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <template>
-    <section id="video"> 
-        <img src="../assets/img/Video.jpg" alt="">
-        <div>
-            <i class="fa-regular fa-circle-play">
-            </i>
-            <h2>NEW MUSIC VIDEO</h2>
-        </div>
-    </section>
+  <section id="video">
+    <img src="../assets/img/Video.jpg" alt="" />
+    <div>
+      <i class="fa-regular fa-circle-play"> </i>
+      <h2>NEW MUSIC VIDEO</h2>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
-
 section {
-    position: relative;
+  position: relative;
+  height: 70vh;
 }
 
 img {
-    width: 100%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
 div {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    color: #f2870c;
-    font-size: 15rem;
-    text-align: center;
-    h2 {
-        color: white;
-    }
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: #f2870c;
+  font-size: 15rem;
+  text-align: center;
+  h2 {
+    color: white;
+  }
 }
-
 </style>
