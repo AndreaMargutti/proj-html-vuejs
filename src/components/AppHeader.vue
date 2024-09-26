8b8f3ea6cec00a8a0383a81fd59d7a434a139876
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-26 14:14:35 +0200

    fixed css in 4 components

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -110,7 +110,7 @@ header {
   top: 0;
   left: 0;
   right: 0;
-  z-index: 1;
+  z-index: 2;
 
   img {
     display: block;
--- src/components/MainTopCard.vue
+++ src/components/MainTopCard.vue
@@ -1,32 +1,34 @@
 <script>
 export default {
   data() {
-    return {
-      
-    }
+    return {};
   },
 
   props: {
     cardInfo: {
       type: Array,
-      required: true
-  }
-},
+      required: true,
+    },
+  },
 
-methods: {
-  getImgPaths: function(imgPath){
-        return new URL(imgPath, import.meta.url).href;
-    }
-}
-}
+  methods: {
+    getImgPaths: function (imgPath) {
+      return new URL(imgPath, import.meta.url).href;
+    },
+  },
+};
 </script>
 
 <template>
   <div id="container">
     <section>
-      <div class="card text-center w-100" v-for="(card, i) in cardInfo" :key="i">
+      <div
+        class="card text-center w-100"
+        v-for="(card, i) in cardInfo"
+        :key="i"
+      >
         <div class="card-body">
-          <img :src="getImgPaths(card.icon)" class="card-img-top">
+          <img :src="getImgPaths(card.icon)" class="card-img-top" />
           <h2 class="card-title">{{ card.title }}</h2>
           <p class="card-text">{{ card.text }}</p>
         </div>
@@ -43,15 +45,13 @@ methods: {
   padding: 30px;
   img {
     filter: invert(100%);
-    height: 200px;
+    height: 95px;
   }
 }
 .card-body {
-  * {
-    margin-bottom: 40px;
-  }
   .card-title {
     text-transform: uppercase;
+    margin: 30px;
   }
   .card-text {
     font-weight: 100;
@@ -61,15 +61,17 @@ methods: {
 
 #container {
   position: relative;
+  width: 100%;
 }
 
 section {
+  width: 95%;
+  gap: 1%;
   display: flex;
-  max-width: 75%;
   margin: 0 auto;
   position: absolute;
   left: 50%;
   top: -50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -15%);
 }
 </style>
