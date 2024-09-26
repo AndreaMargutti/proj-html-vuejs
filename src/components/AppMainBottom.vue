8482c816ea6c171a4e03aab0af20ccf9f6ac4dae
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-26 21:36:55 +0200

    modified blogpost carousel buttons, added method to handle carousel end, overflow hidden to entire document body

--- src/components/AppMainBottom.vue
+++ src/components/AppMainBottom.vue
@@ -1,44 +1,44 @@
 <script>
-import AppMainBottomBlogPost from './AppMainBottomBlogPost.vue';
-import AppMainBottomCarousel from './AppMainBottomCarousel.vue';
+import AppMainBottomBlogPost from "./AppMainBottomBlogPost.vue";
+import AppMainBottomCarousel from "./AppMainBottomCarousel.vue";
 export default {
   data() {
-    return {
-      
-    }
+    return {};
   },
-  components:{
+  components: {
     AppMainBottomBlogPost,
-    AppMainBottomCarousel
-  }
-}
+    AppMainBottomCarousel,
+  },
+};
 </script>
 
 <template>
-    <section id="blog">
-        <h4>MUSIC BLOG</h4>
-        <h2>BEST MUSIC BLOG</h2>
-        <AppMainBottomBlogPost/>
-        <AppMainBottomCarousel/>
-    </section>
+  <section id="blog">
+    <h4>MUSIC BLOG</h4>
+    <h2>BEST MUSIC BLOG</h2>
+    <AppMainBottomBlogPost />
+    <AppMainBottomCarousel />
+  </section>
 </template>
 
 <style lang="scss" scoped>
-#blog{
+#blog {
   background-color: black;
   color: #f2870c;
   height: 1000px;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  h4{
+  h4 {
     text-align: center;
     padding-top: 50px;
   }
-  h2{
+  h2 {
     color: white;
     text-align: center;
     font-size: 3.5rem;
   }
-
 }
 </style>
--- src/components/AppMainBottomBlogPost.vue
+++ src/components/AppMainBottomBlogPost.vue
@@ -1,102 +1,115 @@
 <script>
-import AppMainBottomBlogCard from './AppMainBottomBlogCard.vue';
+import AppMainBottomBlogCard from "./AppMainBottomBlogCard.vue";
 // stabilisco inizio per lo scorrimento al primo elemento array
 const startIndexBlog = 0;
-const maxIndexBlog= 6 - 3 + 1;
+const maxIndexBlog = 6 - 3 + 1;
 const minIndexBlog = 0;
 
 export default {
   data() {
     return {
-      infoCardsBlog:[
+      infoCardsBlog: [
         {
-          imgPath:'../assets/img/hip_hop_wired_to_dance_make.jpg',
-          titlePost:'HIP HOP WIRED TO DANCE',
-          linkPost:'#',
-          imgName:'hip_hop_wired_to_dance_make',
-          datePost:'06 November 2021',
-          overview:'Driving short distances music is a music genre that includes traditional folk music and the contemporary genre that evolved from the former',
+          imgPath: "../assets/img/hip_hop_wired_to_dance_make.jpg",
+          titlePost: "HIP HOP WIRED TO DANCE",
+          linkPost: "#",
+          imgName: "hip_hop_wired_to_dance_make",
+          datePost: "06 November 2021",
+          overview:
+            "Driving short distances music is a music genre that includes traditional folk music and the contemporary genre that evolved from the former",
         },
         {
-          imgPath:'../assets/img/music_business-worldwide.jpg',
-          titlePost:'Music Business Worldwide',
-          linkPost:'#',
-          imgName:'music_business-worldwide',
-          datePost:'27 May  2022',
-          overview:'Accelerate work and drive productivity The Beatles experimentation and creative freedom led other bands to experiment in various ways as well.',
+          imgPath: "../assets/img/music_business-worldwide.jpg",
+          titlePost: "Music Business Worldwide",
+          linkPost: "#",
+          imgName: "music_business-worldwide",
+          datePost: "27 May  2022",
+          overview:
+            "Accelerate work and drive productivity The Beatles experimentation and creative freedom led other bands to experiment in various ways as well.",
         },
         {
-          imgPath:'../assets/img/giveaways_rock.jpg',
-          titlePost:'Giveaways Rock to All',
-          linkPost:'#',
-          imgName:'giveaways_rock',
-          datePost:'07 May  2022',
-          overview:'Signs Your Car Battery Has To Be Replaced When the dashboard lights start flashing, this is a sign the battery is dying. Several issues arise and',
-        },   
+          imgPath: "../assets/img/giveaways_rock.jpg",
+          titlePost: "Giveaways Rock to All",
+          linkPost: "#",
+          imgName: "giveaways_rock",
+          datePost: "07 May  2022",
+          overview:
+            "Signs Your Car Battery Has To Be Replaced When the dashboard lights start flashing, this is a sign the battery is dying. Several issues arise and",
+        },
         {
-          imgPath:'../assets/img/drowned_in_sound_feel_joy.jpg',
-          titlePost:'Drowned In Sound Feel Joy',
-          linkPost:'#',
-          imgName:'drowned_in_sound_feel_joy',
-          datePost:'10 April  2022',
-          overview:'Different Types of Music Content For most music creators, they take their work very seriously because of the emotional attachment they may harbor',
-        },      
+          imgPath: "../assets/img/drowned_in_sound_feel_joy.jpg",
+          titlePost: "Drowned In Sound Feel Joy",
+          linkPost: "#",
+          imgName: "drowned_in_sound_feel_joy",
+          datePost: "10 April  2022",
+          overview:
+            "Different Types of Music Content For most music creators, they take their work very seriously because of the emotional attachment they may harbor",
+        },
         {
-          imgPath:'../assets/img/dancing_astronaut_bounce_ipsum.jpg',
-          titlePost:'Dancing Astronaut Bounce Ipsum',
-          linkPost:'#',
-          imgName:'dancing_astronaut_bounce_ipsum',
-          datePost:'01 March  2022',
-          overview:'Music Promotion Channels There are really a lot of music promotion channels out there. Being a music promotion channel involves a lot of work and',
-        },      
+          imgPath: "../assets/img/dancing_astronaut_bounce_ipsum.jpg",
+          titlePost: "Dancing Astronaut Bounce Ipsum",
+          linkPost: "#",
+          imgName: "dancing_astronaut_bounce_ipsum",
+          datePost: "01 March  2022",
+          overview:
+            "Music Promotion Channels There are really a lot of music promotion channels out there. Being a music promotion channel involves a lot of work and",
+        },
         {
-          imgPath:'../assets/img/consequence_of_sound_make_us.jpg',
-          titlePost:'Consequence of Sound Make us',
-          linkPost:'#',
-          imgName:'consequence_of_sound_make_us',
-          datePost:'14 February  2022',
-          overview:"Electronic Dance Music Rhythm and Blues Before this, R & B was known as 'race music,' a term that originated in the African American",
-        },      
+          imgPath: "../assets/img/consequence_of_sound_make_us.jpg",
+          titlePost: "Consequence of Sound Make us",
+          linkPost: "#",
+          imgName: "consequence_of_sound_make_us",
+          datePost: "14 February  2022",
+          overview:
+            "Electronic Dance Music Rhythm and Blues Before this, R & B was known as 'race music,' a term that originated in the African American",
+        },
         {
-          imgPath:'../assets/img/artist_development_and_production.jpg',
-          titlePost:'Artist Development and Production',
-          linkPost:'#',
-          imgName:'artist_development_and_production',
-          datePost:'07 December  2021',
-          overview:"Characterised by its own beat patterns, the lyrics feature what is often extreme violence snd talk of criminal acts. As a result, there's been",
-        },      
+          imgPath: "../assets/img/artist_development_and_production.jpg",
+          titlePost: "Artist Development and Production",
+          linkPost: "#",
+          imgName: "artist_development_and_production",
+          datePost: "07 December  2021",
+          overview:
+            "Characterised by its own beat patterns, the lyrics feature what is often extreme violence snd talk of criminal acts. As a result, there's been",
+        },
       ],
       currentIndexBlog: startIndexBlog,
-            
-    }
+    };
   },
-  components:{
-    AppMainBottomBlogCard
+  components: {
+    AppMainBottomBlogCard,
   },
-  methods:{
-    nextCard(){
+  methods: {
+    nextCard() {
       if (this.currentIndexBlog < maxIndexBlog) {
         this.currentIndexBlog++;
+      } else {
+        // Se siamo all'ultima card, torna alla prima
+        this.currentIndexBlog = minIndexBlog;
       }
-      console.log('next blog post',this.currentIndexBlog);
+      console.log("next blog post", this.currentIndexBlog);
     },
-    previousCard(){
+    previousCard() {
       if (this.currentIndexBlog > minIndexBlog) {
         this.currentIndexBlog--;
+      } else {
+        // Se siamo alla prima card, vai all'ultima
+        this.currentIndexBlog = maxIndexBlog;
       }
-      console.log('prev blog post',this.currentIndexBlog);
-
-    }, 
-  }, 
-}
+      console.log("prev blog post", this.currentIndexBlog);
+    },
+  },
+};
 </script>
 
 <template>
   <div class="row-blog">
     <!-- vedo solo 3 card alla volta -->
-    <AppMainBottomBlogCard 
-    v-for="(cardItem,index) in 3" :key="index"
-    :cardInfo = "infoCardsBlog[currentIndexBlog + index]"/>
+    <AppMainBottomBlogCard
+      v-for="(cardItem, index) in 3"
+      :key="index"
+      :cardInfo="infoCardsBlog[currentIndexBlog + index]"
+    />
   </div>
   <div class="button-slide">
     <button class="slide-left" @click="previousCard">
@@ -109,18 +122,35 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.row-blog{
+.row-blog {
   display: flex;
   align-items: center;
   flex-wrap: nowrap;
+  width: 90%;
 }
-.button-slide{
+.button-slide {
+  justify-content: space-between;
+  width: 98%;
+  display: flex;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(-2vh, 60vh);
   text-align: center;
 
-  button{
-    margin-right: 20px;
-    background-color: #f2870c;
-
+  button {
+    font-size: 35px;
+    height: 50px;
+    background-color: transparent;
+    color: rgb(63, 63, 63);
+    border: none;
+    &:hover {
+      color: white;
+      scale: 1.2;
+    }
+    &:active {
+      scale: 1;
+    }
   }
 }
 </style>
